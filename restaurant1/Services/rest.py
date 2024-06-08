d5af1d47a87658a62122d8df17ed5dc8921524c8
Hilda-Baraiywo@github.com <baraiywohilda181@gmail.com>
2024-06-08 23:14:36 +0300

    Made a few changes to the invoice.

--- restaurant1/Services/rest.py
+++ restaurant1/Services/rest.py
@@ -9,10 +9,10 @@ def save_time():
     current_time = now.strftime("%H:%M:%S")
     return current_time
 
-@frappe.whitelist(allow_guest=True)
-def calculate_default_value():
-    default_value = 0.00
-    return default_value
+# @frappe.whitelist(allow_guest=True)
+# def calculate_default_value():
+#     default_value = 0.00
+#     return default_value
 
 # @frappe.whitelist(allow_guest=True)
 # def validate_order():
@@ -24,68 +24,9 @@ def calculate_default_value():
 #     return {"valid": True}
 
 @frappe.whitelist(allow_guest=True)
-def calculate_total(customer_id):
+def calculate_total(customer_id='Oloo-02'):
     items = frappe.get_all("Order Item", {"parent": customer_id}, {"price", "quantity"})
     total = 0
     for item in items:
         total += item.get("quantity", 0) * item.get("price", 0)
     return total
-
-# @frappe.whitelist(allow_guest=True)
-# def orderInvoice(order):
-# 	customer_name = order.get('customer_name')
-# 	customer_email = order.get('customer_email')
-
-# 	existing_customer = frappe.get_all("Customer", filters={"email_id": customer_email})
-# 	if existing_customer:
-# 		customer = frapp.get_doc("Customer", existing_customer[0].name)
-# 	else:
-# 		customer = frappe.get_doc({
-# 			'doctype': 'Customer',
-# 			'customer_name': customer_name,
-# 			'email_id': customer_email
-# 		})
-# 		customer.insert()
-
-# 	existing_invoice = frappe.get_all("Sales Invoice", filters={"customer": customer.name, "status": "Draft"})
-# 	if existing_invoice:
-# 		invoice = frappe.get_doc("Sales Invoice", existing_invoice[0].name)
-# 	else:
-# 		invoice = frappe.get_doc({
-# 			"doctype": "Sales Invoice",
-# 			"customer": customer.name,
-# 			"items": [],
-# 		})
-
-# 	for order_item in order.get('items'):
-# 		menu_item = order_item.get('menu_item')
-# 		quantity = order_item.get('quantity')
-# 		price = order_item.get('price')
-
-# 		item_name = menu_item
-# 		existing_item = frappe.get_all("Item", {"item_name": item_name}, {'item_name', 'item_group'})
-# 		if not existing_item:
-# 			item = frappe.get_doc({
-# 				"doctype": "Item",
-# 				"item_code": item_name,
-# 				"item_name": item_name,
-# 				"item_group": "Menu Items",
-# 				"description": "Menu items for restaurant"
-# 			})
-# 			item.insert()
-# 			frappe.db.commit()
-
-# 		existing_item_codes = [item.item_code for item in invoice.get("items")]
-
-# 		if item_name not in existing_item_codes:
-# 			invoice.append("items", {
-# 				"item_code": item_name,
-# 				"rate": price/quantity,
-# 				"amount": price,
-# 				"qty": quantity,
-# 			})
-# 		if not existing_invoice:
-# 			invoice.insert()
-# 		else:
-# 			invoice.save()
-# 		frappe.db.commit()
--- restaurant1/restaurant1/doctype/order/order.py
+++ restaurant1/restaurant1/doctype/order/order.py
@@ -3,13 +3,11 @@
 import frappe
 from frappe.model.document import Document
 
-
 class Order(Document):
 	pass
 
 @frappe.whitelist(allow_guest=True)
-def create_or_update_invoice(customer_id = 'Aloo-01'):
-	print(f"\n\n\n success \n\n\n")
+def create_or_update_invoice(customer_id='Aloo-01'):
 	doc = frappe.get_doc("Order", customer_id)
 	customer_name = doc.customer_name
 
@@ -26,6 +24,16 @@ def create_or_update_invoice(customer_id = 'Aloo-01'):
 		customer = frappe.get_doc("Customer", existing_customer[0].name)
 
 	if doc.order_item:
+		existing_invoice = frappe.get_all("Sales Invoice", filters={"customer": customer.name, "status": "Draft"})
+		if existing_invoice:
+			invoice = frappe.get_doc("Sales Invoice", existing_invoice[0].name)
+		else:
+			invoice = frappe.get_doc({
+				"doctype": "Sales Invoice",
+				"customer": customer.name,
+				"items": [],
+			})
+		existing_item_codes = [item.item_code for item in invoice.get("items")]
 		for order in doc.order_item:
 			existing_item = frappe.db.get_all("Item", {"name": order.name})
 			if not existing_item:
@@ -38,57 +46,13 @@ def create_or_update_invoice(customer_id = 'Aloo-01'):
 				item.insert(ignore_permissions=True)
 				frappe.db.commit()
 
-	existing_invoice = frappe.get_all("Sales Invoice", filters={"customer": customer.name, "status": "Draft"})
-	if existing_invoice:
-		invoice = frappe.get_doc("Sales Invoice", existing_invoice[0].name)
-	else:
-		invoice = frappe.get_doc({
-			"doctype": "Sales Invoice",
-			"customer": customer.name,
-			"items": [],
-		})
-		invoice.insert(ignore_permissions=True)
-		frappe.db.commit()
-
-	existing_item_codes = [item.item_code for item in invoice.get("items")]
-	for order in doc.order_item:
-		if order.name not in existing_item_codes:
-			invoice.append("items", {
-				"item_code": order.name
-			})
+			
+			if order.name not in existing_item_codes:
+				invoice.append("items", {
+					"item_code": order.name,
+					"qty": order.quantity if order.quantity and order.quantity > 0 else 1  # Ensure quantity is set and greater than zero
+					})
 		invoice.save(ignore_permissions=True)
 		frappe.db.commit()
 	return "success"
-
-	# for order_item in order.get('items'):
-	# 	menu_item = order_item.get('menu_item')
-	# 	quantity = order_item.get('quantity')
-	# 	price = order_item.get('price')
-
-		# item_name = menu_item
-		# existing_item = frappe.get_all("Item", {"item_name": item_name}, {'item_name', 'item_group'})
-		# if not existing_item:
-		# 	item = frappe.get_doc({
-		# 		"doctype": "Item",
-		# 		"item_code": item_name,
-		# 		"item_name": item_name,
-		# 		"item_group": "Menu Items",
-		# 		"description": "Menu items for restaurant"
-		# # 	})
-		# 	item.insert()
-		# 	frappe.db.commit()
-
-		# existing_item_codes = [item.item_code for item in invoice.get("items")]
-
-		# if item_name not in existing_item_codes:
-		# 	invoice.append("items", {
-		# 		"item_code": item_name,
-		# 		"rate": price/quantity,
-		# 		"amount": price,
-		# 		"qty": quantity,
-		# 	})
-		# if not existing_invoice:
-		# 	invoice.insert(ignore_permission=True)
-		# else:
-		# 	invoice.save()
-		# frappe.db.commit()
+	
